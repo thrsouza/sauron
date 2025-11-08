4a27ec98be6868e3040729006f9de8850a17153d
Thiago Souza <thiagosouza.me@outlook.com>
2025-11-08 15:21:58 -0300

    chore: initial commit

---
+++ src/test/java/com/github/thrsouza/sauron/SauronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thrsouza.sauron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SauronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
