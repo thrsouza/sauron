e1cfb2c1a0feaf0ae3404910ef9e68bfce200a41
Thiago Souza <thiagosouza.me@outlook.com>
2025-11-09 02:27:24 -0300

    chore: add pre-register jpa repository

--- src/main/java/com/github/thrsouza/sauron/models/PreRegister.java
+++ src/main/java/com/github/thrsouza/sauron/models/PreRegister.java
@@ -4,43 +4,45 @@ import java.time.Instant;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 
+@Entity
 @Getter
+@Setter
 public class PreRegister {
     @Id
-    private final UUID id;
+    private UUID id;
 
     @Column(nullable = false, length = 32, unique = true, updatable = false)
-    private final String document;
+    private String document;
 
     @Column(nullable = false, length = 128, updatable = false)
-    private final String name;
+    private String name;
 
-    @Column(nullable = false, length = 128, unique = true, updatable = false)
-    private final String email;
+    @Column(nullable = false, length = 128, updatable = false)
+    private String email;
 
     @Column(nullable = false, updatable = false)
-    private final Instant createdAt;
+    private Instant createdAt;
 
     @Column(nullable = false)
     private Instant updatedAt;
 
-    private PreRegister(UUID id, String document, String name, String email, Instant createdAt, Instant updatedAt) {
-        this.id = id;
-        this.document = document;
-        this.name = name;
-        this.email = email;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
     public static PreRegister create(String document, String name, String email) {
         UUID id = UUID.randomUUID();
-
         Instant now = Instant.now();
 
-        return new PreRegister(id, document, name, email, now, now);
+        PreRegister preRegister = new PreRegister();
+        preRegister.setId(id);
+        preRegister.setDocument(document);
+        preRegister.setName(name);
+        preRegister.setEmail(email);
+        preRegister.setCreatedAt(now);
+        preRegister.setUpdatedAt(now);
+
+        return preRegister;
     }
 }
